3e9751e212958cf4352ce4c222d238855d922599
kedir <kedirhamid@gmail.com>
2019-07-31 22:12:39 -0400

    run script file

---
+++ run_script.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 21:48:50 2019
+
+@author: Kedir
+"""
+from colab_util import *
+from google.colab import files
+import glob 
+import svnh_semi_supervised_model_train as train
+import zipfile
+
+train.main()
+model_files = sorted(glob.glob('./checkpoints/*'))
+
+zipf = zipfile.ZipFile('checkpoints.zip', 'w', zipfile.ZIP_DEFLATED)
+for file in model_files:
+    zipf.write(file)
+
+files.download('./checkpoins.zip')
